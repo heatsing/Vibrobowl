ccdb8c2032b836543654db43f44cb7736a566e3f
heatsing <heatsing@users.noreply.github.com>
2026-06-05 15:19:04 +0800

    Use HTTPS apex domain as canonical URL

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://vibrobowl.com";
+export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://vibrobowl.com";
